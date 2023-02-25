87c39d026a36aa13ae0524699d2222af18e4414c
renanspvargas <renanspvargas@outlook.com>
2023-02-25 18:49:25 -0300

    app bar component

---
+++ lib/app/core/ui/widgets/vakinha_appbar.dart
@@ -0,0 +1,18 @@
+import 'package:adf_vakinhaburger_getx/app/core/ui/constants/vakinha_images.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class VakinhaAppbar extends AppBar {
+  VakinhaAppbar({Key? key, double elevation = 2})
+      : super(
+          key: key,
+          backgroundColor: Colors.white,
+          elevation: elevation,
+          centerTitle: true,
+          title: Image.asset(
+            VakinhaImages.imagelogo,
+            width: 80,
+          ),
+          iconTheme: const IconThemeData(color: Colors.black),
+        );
+}
