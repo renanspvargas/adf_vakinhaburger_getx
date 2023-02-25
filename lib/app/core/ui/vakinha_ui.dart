255dd6ecccce5dc2f2f012d3cd1c24fe5ee39f5e
renanspvargas <renanspvargas@outlook.com>
2023-02-25 16:10:00 -0300

    creating theme

---
+++ lib/app/core/ui/vakinha_ui.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VakinhaUI {
+  VakinhaUI._();
+
+  static final theme = ThemeData(
+    primaryColor: const Color(0xFF007D21),
+    primaryColorDark: const Color(0xFF00343F),
+  );
+}
