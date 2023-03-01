c20b06cd674d62e0abb1fd0ac10481598db3a627
renanspvargas <renanspvargas@outlook.com>
2023-03-01 15:57:17 -0300

    finished login page

--- lib/app/modules/auth/login/login_page.dart
+++ lib/app/modules/auth/login/login_page.dart
@@ -1,4 +1,9 @@
+import 'package:adf_vakinhaburger_getx/app/core/ui/vakinha_ui.dart';
+import 'package:adf_vakinhaburger_getx/app/core/ui/widgets/vakinha_appbar.dart';
+import 'package:adf_vakinhaburger_getx/app/core/ui/widgets/vakinha_button.dart';
+import 'package:adf_vakinhaburger_getx/app/core/ui/widgets/vakinha_textformfield.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -6,10 +11,61 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
+      appBar: VakinhaAppbar(
+        elevation: 0,
       ),
-      body: Container(),
+      backgroundColor: Colors.white,
+      body: LayoutBuilder(builder: (_, constraints) {
+        return ConstrainedBox(
+          constraints: BoxConstraints(minHeight: constraints.maxHeight),
+          child: IntrinsicHeight(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Login',
+                      style: context.textTheme.headlineSmall?.copyWith(
+                        fontWeight: FontWeight.bold,
+                        color: context.theme.primaryColorDark,
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    const VakinhaTextformfield(label: 'Email'),
+                    const SizedBox(height: 30),
+                    const VakinhaTextformfield(
+                        label: 'Senha', obscureText: true),
+                    const SizedBox(height: 50),
+                    Center(
+                      child: VakinhaButton(
+                        label: 'ENTRAR',
+                        width: context.width,
+                        onPressed: () {},
+                      ),
+                    ),
+                    const Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text('Não possui uma conta?'),
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Cadastre-se',
+                            style: VakinhaUI.textBold,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      }),
     );
   }
 }
