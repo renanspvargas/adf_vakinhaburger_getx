e03296e2dcfb9e59f2cfb9a6388f712e63261b87
renanspvargas <renanspvargas@outlook.com>
2023-03-01 19:05:06 -0300

    creating register method on controller

--- lib/app/modules/auth/register/register_controller.dart
+++ lib/app/modules/auth/register/register_controller.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:adf_vakinhaburger_getx/app/core/mixins/loader_mixin.dart';
 import 'package:adf_vakinhaburger_getx/app/core/mixins/messages_mixin.dart';
+import 'package:adf_vakinhaburger_getx/app/core/rest_client/rest_client.dart';
 import 'package:adf_vakinhaburger_getx/app/repositories/auth/auth_repository.dart';
 import 'package:get/get.dart';
 
@@ -18,5 +21,41 @@ class RegisterController extends GetxController
     loaderListener(_loading);
     messageListener(_message);
     super.onInit();
+  }
+
+  Future<void> register({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      _loading.toggle();
+      final userModel = await _authRepository.register(
+        name: name,
+        email: email,
+        password: password,
+      );
+      _message(MessageModel(
+        title: 'Sucesso',
+        message: 'Cadastro realizado com sucesso!',
+        type: MessageType.info,
+      ));
+    } on RestClientException catch (e, s) {
+      _loading.toggle();
+      log('Erro ao registrar usuario', error: e, stackTrace: s);
+      _message(MessageModel(
+        title: 'Erro',
+        message: e.message,
+        type: MessageType.error,
+      ));
+    } catch (e, s) {
+      _loading.toggle();
+      log('Erro ao registrar usuario', error: e, stackTrace: s);
+      _message(MessageModel(
+        title: 'Erro',
+        message: "Erro ao registrar usuario",
+        type: MessageType.error,
+      ));
+    }
   }
 }
