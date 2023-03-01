62554110d8e217ce6a5e1185d5058235d4636c81
renanspvargas <renanspvargas@outlook.com>
2023-03-01 18:33:21 -0300

    using mixin on register controller

--- lib/app/modules/auth/register/register_controller.dart
+++ lib/app/modules/auth/register/register_controller.dart
@@ -1,9 +1,22 @@
+import 'package:adf_vakinhaburger_getx/app/core/mixins/loader_mixin.dart';
+import 'package:adf_vakinhaburger_getx/app/core/mixins/messages_mixin.dart';
 import 'package:adf_vakinhaburger_getx/app/repositories/auth/auth_repository.dart';
 import 'package:get/get.dart';
 
-class RegisterController extends GetxController {
+class RegisterController extends GetxController
+    with LoaderMixin, MessagesMixin {
   final AuthRepository _authRepository;
+
+  final _loading = false.obs;
+  final _message = Rxn<MessageModel>();
 
   RegisterController({required AuthRepository authRepository})
       : _authRepository = authRepository;
+
+  @override
+  void onInit() {
+    loaderListener(_loading);
+    messageListener(_message);
+    super.onInit();
+  }
 }
