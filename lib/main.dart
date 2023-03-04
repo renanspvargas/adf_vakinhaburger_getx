69ea632982d6dfba79a11df6e39f5eeeee7be3e6
renanspvargas <renanspvargas@outlook.com>
2023-03-04 13:20:03 -0300

    init GetStorage before the app

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:adf_vakinhaburger_getx/app/core/ui/vakinha_ui.dart';
 import 'package:adf_vakinhaburger_getx/app/routes/auth_routes.dart';
 import 'package:adf_vakinhaburger_getx/app/routes/splash_routers.dart';
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:get/get.dart';
 
-void main() {
+Future<void> main() async {
+  await GetStorage.init();
   runApp(const VakinhaBugerMainApp());
 }
 
